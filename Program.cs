a44a6991e2d1fa3ee9f29effc85302e729078bdb
Elis Wagner Tornqvist <elis.wagnertornkvist@elev.ga.ntig.se>
2024-11-12 12:13:46 +0100

    näsan över E

---
+++ Program.cs
@@ -0,0 +1,117 @@
+﻿// if(6>=3)
+// {
+// Console.WriteLine("Hello, World!");
+// Console.ReadLine();
+// }
+
+
+//Uppgift 2
+// string name = Console.ReadLine() ?? string.Empty;
+// if(name.ToLower() == "noname")
+// {
+//     Console.WriteLine("Welcome!");
+//     Console.ReadLine();
+// }
+
+//Uppgift 3
+// Console.WriteLine("Användarnamn och lösenord tack");
+// string name = Console.ReadLine() ?? string.Empty;
+// string password = Console.ReadLine() ?? string.Empty;
+// if(name.ToLower() == "noname" && password.ToLower() == "nopass")
+// {
+//     Console.WriteLine("Welcome!");
+
+// } else {
+//     Console.WriteLine("Wrong username or password");
+
+// }
+
+// Console.ReadLine();
+
+
+//uppgift 4
+// for (int i = 0; i < 32; i++)   
+// {
+//     Console.WriteLine("Hello World!");
+// }
+
+// Console.ReadLine();
+
+
+//uppgift 5 
+
+// bool answer = true;
+
+// while(answer == true)
+// {
+
+
+// Console.WriteLine("Användarnamn och lösenord tack");
+// string name = Console.ReadLine() ?? string.Empty;
+// string password = Console.ReadLine() ?? string.Empty;
+// if(name.ToLower() == "noname" && password.ToLower() == "nopass")
+// {
+//     Console.WriteLine("Welcome!");
+//     answer = false;
+
+// } else {
+//     Console.WriteLine("Wrong username or password");
+
+// }
+
+// }
+
+// Console.ReadLine();
+
+
+//uppgift 6
+
+// for(int i = 0; i < 5; i++)
+// {
+//     Console.WriteLine("Skirv ett tal");
+//     string number = Console.ReadLine() ?? string.Empty;
+//     int numberInt = 0;
+//     int.TryParse(number, out numberInt);
+
+//     if(numberInt > 5)
+//     {
+//         Console.WriteLine("Högre än 5!");
+//     }
+
+// }
+
+// Console.ReadLine();
+
+
+// uppgift 7
+
+
+// while (true)
+// {
+
+//     Console.WriteLine("Skriv något");
+
+//     string answer = Console.ReadLine() ?? string.Empty;
+//     int answerInt = 0;
+//     bool success = int.TryParse(answer, out answerInt);
+
+//     if (success == true)
+//     {
+//         Console.WriteLine("blev rätt");
+//         break;
+//     }
+
+//     if (success != true)
+//     {
+//         Console.WriteLine("blev fel");
+
+//     }
+// }
+
+
+//uppgift 8, sista
+
+
+
+int randomNumber = Random.Shared.Next(1, 10);
+
